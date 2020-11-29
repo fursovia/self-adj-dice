36b65be03c531442cfe377fbdf698028f118fc30
fursovia <fursov.ia@gmail.com>
2020-11-29 17:45:50 +0300

    added reduction argument

--- sadice/loss.py
+++ sadice/loss.py
@@ -9,16 +9,21 @@ class SelfAdjDiceLoss(torch.nn.Module):
     Args:
         alpha (float): a factor to push down the weight of easy examples
         gamma (float): a factor added to both the nominator and the denominator for smoothing purposes
+        reduction (string): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will be applied,
+            ``'mean'``: the sum of the output will be divided by the number of
+            elements in the output, ``'sum'``: the output will be summed.
 
     Shape:
         - logits: `(N, C)` where `N` is the batch size and `C` is the number of classes.
         - targets: `(N)` where each value is in [0, C - 1]
     """
 
-    def __init__(self, alpha: float = 1.0, gamma: float = 1.0) -> None:
+    def __init__(self, alpha: float = 1.0, gamma: float = 1.0, reduction: str = "mean") -> None:
         super().__init__()
         self.alpha = alpha
         self.gamma = gamma
+        self.reduction = reduction
 
     def forward(self, logits: torch.Tensor, targets: torch.Tensor) -> torch.Tensor:
         probs = torch.softmax(logits, dim=1)
@@ -26,4 +31,12 @@ class SelfAdjDiceLoss(torch.nn.Module):
 
         probs_with_factor = ((1 - probs) ** self.alpha) * probs
         loss = 1 - (2 * probs_with_factor + self.gamma) / (probs_with_factor + 1 + self.gamma)
-        return loss.mean()
+
+        if self.reduction == "mean":
+            return loss.mean()
+        elif self.reduction == "sum":
+            return loss.sum()
+        elif self.reduction == "none" or self.reduction is None:
+            return loss
+        else:
+            raise NotImplementedError(f"Reduction `{self.reduction}` is not supported.")
